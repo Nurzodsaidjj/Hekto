ca8a58f6babfd097c0fb6b65cc63055c5f706e2f
Nurzodsaidjj <nurzodsaidjonov7@gmail.com>
2025-06-29 20:02:21 +0500

    add javascript tabs

---
+++ data.js
@@ -0,0 +1,277 @@
+export const category = [
+  "Washing mashine",
+  "Phone",
+  "Laptops",
+  "Tv",
+  "Keyboard",
+  "Scuter",
+];
+export const products = [
+  {
+    title: "Washing mashine",
+    products: [
+      {
+        id: 1,
+        name: "Washing Machine 1",
+        price: 4102000,
+        img: "https://pngimg.com/d/washing_machine_PNG15589.png",
+      },
+      {
+        id: 2,
+        name: "Washing Machine 2",
+        price: 6000000,
+        img: "https://pngimg.com/d/washing_machine_PNG15589.png",
+      },
+      {
+        id: 3,
+        name: "Washing Machine 3",
+        price: 29000000,
+        img: "https://pngimg.com/d/washing_machine_PNG15589.png",
+      },
+      {
+        id: 4,
+        name: "Washing Machine 4",
+        price: 6000000,
+        img: "https://pngimg.com/d/washing_machine_PNG15589.png",
+      },
+      {
+        id: 5,
+        name: "Washing Machine 5",
+        price: 7000000,
+        img: "https://pngimg.com/d/washing_machine_PNG15589.png",
+      },
+      {
+        id: 6,
+        name: "Ziffler T80-DINTS 8kg",
+        price: 4102000,
+        monthlyPrice: 481000,
+        img: "https://pngimg.com/d/washing_machine_PNG15589.png",
+      },
+    ],
+  },
+  {
+    title: "Phone",
+    products: [
+      {
+        id: 6,
+        name: "Xiaomi Redmi Note 14 Pro",
+        price: 3700000,
+        img: "https://static.vecteezy.com/system/resources/previews/035/572/095/non_2x/samsung-galaxy-s23-ultra-black-transparent-image-free-png.png",
+      },
+      {
+        id: 7,
+        name: "Apple iPhone 15 Pro Max",
+        price: 14126000,
+        img: "https://static.vecteezy.com/system/resources/previews/035/572/095/non_2x/samsung-galaxy-s23-ultra-black-transparent-image-free-png.png",
+      },
+      {
+        id: 8,
+        name: "Redmi Note 14",
+        price: 2176000,
+        img: "https://static.vecteezy.com/system/resources/previews/035/572/095/non_2x/samsung-galaxy-s23-ultra-black-transparent-image-free-png.png",
+      },
+      {
+        id: 9,
+        name: "Samsung Galaxy S25 Ultra",
+        price: 13440000,
+        img: "https://static.vecteezy.com/system/resources/previews/035/572/095/non_2x/samsung-galaxy-s23-ultra-black-transparent-image-free-png.png",
+      },
+      {
+        id: 10,
+        name: "Samsung Galaxy A55",
+        price: 4511800,
+        img: "https://static.vecteezy.com/system/resources/previews/035/572/095/non_2x/samsung-galaxy-s23-ultra-black-transparent-image-free-png.png",
+      },
+      {
+        id: 11,
+        name: "Xiaomi Redmi 14C",
+        price: 1440000,
+        img: "https://static.vecteezy.com/system/resources/previews/035/572/095/non_2x/samsung-galaxy-s23-ultra-black-transparent-image-free-png.png",
+      },
+      {
+        id: 12,
+        name: "Apple iPhone 16 Pro Max",
+        price: 16350000,
+        img: "https://static.vecteezy.com/system/resources/previews/035/572/095/non_2x/samsung-galaxy-s23-ultra-black-transparent-image-free-png.png",
+      },
+    ],
+  },
+  {
+    title: "Laptops",
+    products: [
+      {
+        id: 13,
+        img: "https://spaces-cdn.clipsafari.com/ko71bin8za9ckshsej6v5k02kkoo",
+        name: "notbook111",
+        prise: 20760225,
+      },
+      {
+        id: 14,
+        img: "https://spaces-cdn.clipsafari.com/ko71bin8za9ckshsej6v5k02kkoo",
+        name: "notbook222",
+        prise: 20760225,
+      },
+      {
+        id: 15,
+        img: "https://spaces-cdn.clipsafari.com/ko71bin8za9ckshsej6v5k02kkoo",
+        name: "notbook333",
+        prise: 22760225,
+      },
+      {
+        id: 16,
+        img: "https://spaces-cdn.clipsafari.com/ko71bin8za9ckshsej6v5k02kkoo",
+        name: "notbook444",
+        prise: 24764225,
+      },
+      {
+        id: 17,
+        img: "https://spaces-cdn.clipsafari.com/ko71bin8za9ckshsej6v5k02kkoo",
+        name: "notbook555",
+        prise: 23760225,
+      },
+      {
+        id: 18,
+        img: "https://spaces-cdn.clipsafari.com/ko71bin8za9ckshsej6v5k02kkoo",
+        name: "notbook666",
+        prise: 21760225,
+      },
+    ],
+  },
+  {
+    title: "Tv",
+    products: [
+      {
+        name: 'Телевизор Ziffler 100QW900U 100"',
+        price: 28006000,
+        monthly: 3280000,
+        size: "100 inch",
+        id: 19,
+        img: "https://static.vecteezy.com/system/resources/previews/038/015/883/non_2x/ai-generated-modern-tv-isolated-on-transparent-background-free-png.png",
+      },
+      {
+        id: 20,
+        name: 'Телевизор Ziffler 86QW700U 86"',
+        price: 13281000,
+        monthly: 1556000,
+        size: "86 inch",
+        img: "https://static.vecteezy.com/system/resources/previews/038/015/883/non_2x/ai-generated-modern-tv-isolated-on-transparent-background-free-png.png",
+      },
+      {
+        id: 21,
+        name: 'Телевизор Ziffler 85W500U 85"',
+        price: 11747000,
+        monthly: 1376000,
+        size: "85 inch",
+        img: "https://static.vecteezy.com/system/resources/previews/038/015/883/non_2x/ai-generated-modern-tv-isolated-on-transparent-background-free-png.png",
+      },
+      {
+        id: 22,
+        name: 'Телевизор Ziffler 75W500U 75"',
+        price: 7990000,
+        monthly: 930000,
+        size: "75 inch",
+        img: "https://static.vecteezy.com/system/resources/previews/038/015/883/non_2x/ai-generated-modern-tv-isolated-on-transparent-background-free-png.png",
+      },
+      {
+        id: 23,
+        name: 'Телевизор Ziffler 65W500U 65"',
+        price: 5990000,
+        monthly: 700000,
+        size: "65 inch",
+        img: "https://static.vecteezy.com/system/resources/previews/038/015/883/non_2x/ai-generated-modern-tv-isolated-on-transparent-background-free-png.png",
+      },
+      {
+        id: 24,
+        name: 'Телевизор Ziffler 55W500U 55"',
+        price: 3990000,
+        monthly: 460000,
+        size: "55 inch",
+        img: "https://static.vecteezy.com/system/resources/previews/038/015/883/non_2x/ai-generated-modern-tv-isolated-on-transparent-background-free-png.png",
+      },
+    ],
+  },
+  {
+    title: "Keyboard",
+    products: [
+      {
+        id: 25,
+        name:
+          "Игровая проводная Gaming Keyboard (639N9AA#ABA)",
+        price: 1299000,
+        img: "https://olcha.uz/image/700x700/products//supplier/stores/27247/hyperx-alloy-origins-core-pbt%2F1.png",
+      },
+      {
+        id: 26,
+        name: "Клавиатура Pwnage Ultra Сustom White - Blue Switches",
+        price: 1152000,
+        img: "https://olcha.uz/image/700x700/products//supplier/stores/27247/hyperx-alloy-origins-core-pbt%2F1.png",
+      },
+      {
+        id: 27,
+        name: "Игровая клавиатура HyperX Alloy Rise 75",
+        price: 2197000,
+        img: "https://olcha.uz/image/700x700/products//supplier/stores/27247/hyperx-alloy-origins-core-pbt%2F1.png",
+      },
+      {
+        id: 27,
+        name: "Игровая клавиатура Dark Project KD68 Sunrise",
+        price: 1211000,
+        img: "https://olcha.uz/image/700x700/products//supplier/stores/27247/hyperx-alloy-origins-core-pbt%2F1.png",
+      },
+      {
+        id: 28,
+        name:
+          "Игровая клавиатура Razer BlackWidow V4 Pro Layout",
+        price: 2425626,
+        img: "https://olcha.uz/image/700x700/products//supplier/stores/27247/hyperx-alloy-origins-core-pbt%2F1.png",
+      },
+      {
+        id: 29,
+        name: "Игровая клавиатура Razer Ornata V3 X (Russian Layout) Black",
+        price: 75000,
+        img: "https://olcha.uz/image/700x700/products//supplier/stores/27247/hyperx-alloy-origins-core-pbt%2F1.png",
+      },
+    ],
+  },
+  {
+    title: "Scuter",
+    products: [
+      {
+        id: 30,
+        name: "Ролики гелевые универсальные раздвижные 39-43 размер",
+        img: "https://pngimg.com/uploads/scooter/scooter_PNG11335.png",
+        price: 7567900,
+      },
+      {
+        id: 31,
+        name: "Электрический скутер Bonvi 7157",
+        img: "https://pngimg.com/uploads/scooter/scooter_PNG11335.png",
+        price: 1246075,
+      },
+      {
+        id: 32,
+        name: "Скутер бензиновый BONVI 4796",
+        img: "https://pngimg.com/uploads/scooter/scooter_PNG11335.png",
+        price: 1279595,
+      },
+      {
+        id: 33,
+        name: 'Монитор Xiaomi RMMNT238NF 24" FHD',
+        img: "https://pngimg.com/uploads/scooter/scooter_PNG11335.png",
+        price: 150000,
+      },
+      {
+        id: 34,
+        name: "RGB контроллер GameMax PWM+ARGB HUB V2.0 (ARGBHUB2.0)",
+        img: "https://pngimg.com/uploads/scooter/scooter_PNG11335.png",
+        price: 168235,
+      },
+      {
+        id: 35,
+        name: 'Монитор Xiaomi RMMNT238NF 24" FHD',
+        img: "https://pngimg.com/uploads/scooter/scooter_PNG11335.png",
+        price: 1279595,
+      },
+    ],
+  },
+];
--- main.js
+++ main.js
@@ -1,11 +1,67 @@
-const open__modal = document.querySelector('.open__modal');
-const modal = document.querySelector('.modal');
-const close_btn = document.querySelector('.close_btn');
+const saveStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+const getStorage = (key) => {
+  const data = JSON.parse(localStorage.getItem(key));
+  return data;
+};
 
-open__modal.addEventListener('click', () => {
-  modal.classList.add('active');
+const themeAction = document.querySelector(".theme__btn");
+const body = document.querySelector("body");
+
+(() => {
+  const theme = getStorage("theme");
+  if (theme) {
+    body.classList.add(theme);
+  }
+  body.classList.add();
+})();
+
+themeAction.addEventListener("click", () => {
+  const data = body.classList.toggle("dark");
+  if (data) {
+    saveStorage("theme", "dark");
+  } else {
+    saveStorage("theme", "light");
+  }
 });
+//////////////////////////////////////tabs//////////////////////////////////////
 
-close_btn.addEventListener('click', () => {
-  modal.classList.remove('active');
+import { category, products } from "./data.js";
+const leatest__list = document.querySelector(".leatest__list");
+const product__box = document.querySelector(".product__box");
+
+const renderProduct = (key) => {
+  const data = products.find((item) => item.title === key).products;
+  product__box.innerHTML = data
+    .map(
+      (obj) => `
+    <div>
+    <img src="${obj.img}" alt="img" />
+    <h2 class="leatest__item">${obj.name}</h2>
+    <strong>${obj.price}</strong>
+    </div>`
+    )
+    .join("");
+};
+const renderCategory = (active) => {
+  leatest__list.innerHTML = category
+    .map(
+      (item) => `
+    <li data-title="${item}" class="leatest__item ${
+        item === active ? "active__tab" : ""
+      }"> ${item} </li>
+    `
+    )
+    .join("");
+};
+renderProduct(category[0]);
+renderCategory(category[0]);
+
+leatest__list.addEventListener("click", (e) => {
+  if (e.target.dataset.title) {
+    const title = e.target.dataset.title;
+    renderProduct(e.target.dataset.title);
+    renderCategory(e.target.dataset.title);
+  }
 });
